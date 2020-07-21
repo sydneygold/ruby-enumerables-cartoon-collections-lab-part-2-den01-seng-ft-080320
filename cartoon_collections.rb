7b66db3039447e1927e7e22709b9945b70519095
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-21 22:37:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 planeteer_calls.include? do |element|
-  valid_calls
+  valid_calls[2]
   puts element
 end
 end
